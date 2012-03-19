409e893e1aae69877c64db1ebeeb4ad8d464908c
Ian Young <ian.greenleaf@gmail.com>
2012-03-18 23:01:04 -0700

    Raise exceptions on save failures

--- config/boot.rb
+++ config/boot.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 Dir["./lib/**/*.rb"].each { |f| require f }
 
 DataMapper::Logger.new($stdout, :debug) if ENV["RACK_ENV"] == "development"
+DataMapper::Model.raise_on_save_failure = true
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
 DataMapper.auto_upgrade!
 
