ea79e2d309c4709a8ea606830eff58416b57eb33
Ian Young <ian.greenleaf@gmail.com>
2013-10-10 12:51:29 -0700

    Default DM strings to 255 chars

--- config/boot.rb
+++ config/boot.rb
@@ -5,10 +5,12 @@ Bundler.setup
 
 Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
+DataMapper::Logger.new($stdout, :debug) if ENV["RACK_ENV"] == "development"
+DataMapper::Property::String.length(255)
+DataMapper::Model.raise_on_save_failure = false
+
 Dir["./lib/**/*.rb"].each { |f| require f }
 
-DataMapper::Logger.new($stdout, :debug) if ENV["RACK_ENV"] == "development"
-DataMapper::Model.raise_on_save_failure = true
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
 DataMapper.auto_upgrade!
 
